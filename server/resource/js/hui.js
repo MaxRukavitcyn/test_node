cccc1833ab650960c77a6d36e08e55c5ce78b02d
MaxHand <rukav77@gmail.com>
2019-06-28 00:00:01 +0500

    fix

--- server/resource/js/hui.js
+++ server/resource/js/hui.js
@@ -23,11 +23,11 @@ class Array2 {
 }
 let f = (res, key)=>key.split('.').reduce((acc, key)=>acc && acc[key]? acc[key] : undefined, res);
 // let emp1 = f(sor, 'emp2.f1.q1');
-console.log(emp1);
-
-emp1 = sor.emp2.f1.q1;
-console.log(emp1);
-
-console.log(f(sor, 'emp4.f1.q1')); //undefined
+// console.log(emp1);
+//
+// emp1 = sor.emp2.f1.q1;
+// console.log(emp1);
+//
+// console.log(f(sor, 'emp4.f1.q1')); //undefined
 
 // console.log(sor.emp4.f1.q1) //error
