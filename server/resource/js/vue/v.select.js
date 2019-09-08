b10879e0960faae128eb804578036b576438d2d2
MaxHand <rukav77@gmail.com>
2019-09-09 00:29:09 +0500

    fix

--- server/resource/js/vue/v.select.js
+++ server/resource/js/vue/v.select.js
@@ -4,7 +4,7 @@ let template = `<div>
 							<input @click="open" readonly :value="val.name">
 							<span v-if="val" style="position: relative;top: -17px;left: 195px;cursor: pointer; color: red" @click="clean">x</span>
 						</div>
-						<div v-show="show" style="width: 210px;position: relative;top: -15px;background: white;border: black 1px solid;">
+						<div v-show="show" style="width: 210px;position: relative;top: 2px;background: white;border: black 1px solid;">
 						<span v-if="!model || model.list.length === 0" style="position: relative;left: 60px;">нет данных</span>
 							<ul class="select-list">
 								<li v-for="(l, index) in list">
