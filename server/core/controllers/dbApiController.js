d34b3088f2864febb18f08a2816e93e97c4cd72a
MaxHand <rukav77@gmail.com>
2019-10-08 01:59:04 +0500

    test deploy postgres

--- server/core/controllers/dbApiController.js
+++ server/core/controllers/dbApiController.js
@@ -1,22 +1,30 @@
-const pdb = require("../db/connect");
+// const pdb = require("../db/connect");
 
 const express = require('express');
 const cors = require('cors');
 
 const paths = express();
+const { Client } = require('pg');
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true,
+});
+
+client.connect();
 
 let corsOptions = {
 	origins: ['http://localhost:8080', 'http://localhost'],
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 paths.get('/db/test', cors(corsOptions), (request, response) => {
-	pdb.pdb.any('select * from node_test.public.test_table').then(data=>{
+	client.query('select * from public.test_table').then(data=>{
 		response.send(data)
 	});
 });
 paths.post('/db/test/post', cors(corsOptions), (request, response) =>{
 	let body = request.body;
-	pdb.pdb.any('insert into node_test.public.test_table(first_name, last_name) values ($1, $2) returning id, first_name, last_name', [body.firstName, body.lastName]).then(data=>{
+	client.query('insert into public.test_table(first_name, last_name) values ($1, $2) returning id, first_name, last_name', [body.firstName, body.lastName]).then(data=>{
 		response.send(data);
 	});
 });
