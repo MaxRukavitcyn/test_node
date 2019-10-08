17f0c19bf9980d154bd4e6816d9c5178664ccb38
MaxHand <rukav77@gmail.com>
2019-10-08 21:09:43 +0500

    test deploy postgres

--- server/core/controllers/dbApiController.js
+++ server/core/controllers/dbApiController.js
@@ -10,13 +10,13 @@ let corsOptions = {
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 paths.get('/db/test', cors(corsOptions), (request, response) => {
-	pdb.pdb.any('select * from node_test.public.test_table').then(data=>{
+	pdb.client.any('select * from test_table').then(data=>{
 		response.send(data)
 	});
 });
 paths.post('/db/test/post', cors(corsOptions), (request, response) =>{
 	let body = request.body;
-	pdb.pdb.any('insert into node_test.public.test_table(first_name, last_name) values ($1, $2) returning id, first_name, last_name', [body.firstName, body.lastName]).then(data=>{
+	pdb.client.any('insert into test_table(first_name, last_name) values ($1, $2) returning id, first_name, last_name', [body.firstName, body.lastName]).then(data=>{
 		response.send(data);
 	});
 });
