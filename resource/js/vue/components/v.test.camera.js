3e7e692b8be1189886f704fca8478a44e90db5ee
MaxHand <rukav77@gmail.com>
2020-12-12 03:55:38 +0500

    test web camera

--- resource/js/vue/components/v.test.camera.js
+++ resource/js/vue/components/v.test.camera.js
@@ -10,10 +10,19 @@ export let vTestCamera = {
 		return {}
 	},
 	mounted() {
-		navigator.webkitGetUserMedia('audio, video', function(localMediaStream) {
-			var video = document.getElementById('video');
-			video.src = window.webkitURL.createObjectURL(localMediaStream);
-		}, onFailSoHard);
+		var video = document.querySelector('video');
+		
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({audio: true, video: true}, function(stream) {
+				video.src = stream;
+			}, onFailSoHard);
+		} else if (navigator.webkitGetUserMedia) {
+			navigator.webkitGetUserMedia('audio, video', function(stream) {
+				video.src = window.webkitURL.createObjectURL(stream);
+			}, onFailSoHard);
+		} else {
+			video.src = 'somevideo.webm'; // fallback.
+		}
 		
 		function onFailSoHard() {}
 	}
